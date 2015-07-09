27e9f3f585994cfdeccbe1e1421243b295190da0
Michael Tran <mtran45@gmail.com>
2015-07-09 13:38:59 +1000

    Finish edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,24 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
   
   private
-  
+
+    # Strong params to prevent mass assignment vulnerability
     def user_params
       params.require(:user).permit(:name, :email, :password, 
                                     :password_confirmation)
