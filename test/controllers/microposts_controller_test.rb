5b2edd4b9d685d350c14475dcfccd5cec02ee22c
Michael Tran <mtran45@gmail.com>
2015-07-11 08:05:44 +1000

    Finish up to 11.3.5

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @micropost = microposts(:orange)
+  end
+
+  test "should redirect CREATE when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      post :create, micropost: { content: "Lorem ipsum" }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect DESTROY when not logged in" do
+    assert_no_difference 'Micropost.count' do
+      delete :destroy, id: @micropost
+    end
+    assert_redirected_to login_url
+  end
 end
